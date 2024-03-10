cfa4e06d2dab6305e297455b557e90fa2a1f1fe3
Cayo さみしい <144740711+cayodka@users.noreply.github.com>
2024-03-10 15:45:14 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+var cat= dormir;
+var one= Number;
+
+var input;
+if (input === undefined) {
+  facaIsto();
+} else {
+  facaAquilo();
+}
+
+var n = null;
+console.log(n * 32)
+
+var myArray = [];
+if (!myArray[0]) myFunction();
